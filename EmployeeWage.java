82b969565de3f3b7fdc6d6d9b7232a38324bf4a8
Rajendhar9515 <vemula.rajendhar1@gmail.com>
2022-07-27 17:12:00 +0530

    check condition

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,26 +4,28 @@ public class EmployeeWage {
 	public static final int isPartTime = 1;
 	public static final int isFullTime = 2;
 	public static final int empRatePerHr = 20;
-	public static final int numOfWorkingDays = 2;
+	public static final int numOfWorkingDays = 1;
+	public static final int maxHrsInMonth = 10;
 
 	public static void main(String[] args) {
-		int empHrs = 0, empWage = 0, totalEmpWage = 0;
-		for (int day = 0; day < numOfWorkingDays; day++) {
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays < numOfWorkingDays) {
+			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
-			case (isPartTime):
+			case isPartTime:
 				empHrs = 4;
 				break;
-			case (isFullTime):
+			case isFullTime:
 				empHrs = 8;
 				break;
 			default:
 				empHrs = 0;
 			}
-			empWage = empHrs * empRatePerHr;
-			totalEmpWage += empWage;
-			System.out.println("empWage : " + empWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Day#: " + totalWorkingDays + " empHr:" + empHrs);
 		}
-		System.out.println("totalEmpWage : " + totalEmpWage);
+		int totalempWage = totalEmpHrs + empRatePerHr;
+		System.out.println("Total empWage: " + totalempWage);
 	}
 }
