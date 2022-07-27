f34390c92918a68ea3bbee13bbdf8073e5f7c16c
Rajendhar9515 <vemula.rajendhar1@gmail.com>
2022-07-27 16:45:11 +0530

    employee present or absent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,15 @@
 package com.bridgelabz.employeewage;
 
 public class EmployeeWage {
+	public static void main(String[] args) {
+		int isFullTime = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == isFullTime) {
+			System.out.println("Employ is Prasent");
+		} else {
+			System.out.println("Employ is Absent");
+		}
+
+	}
 
 }
