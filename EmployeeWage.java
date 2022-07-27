5eca2877f97d54fc893ec5289df7723360b36e3a
Rajendhar9515 <vemula.rajendhar1@gmail.com>
2022-07-27 17:00:06 +0530

    part time employee wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,13 +2,16 @@ package com.bridgelabz.employeewage;
 
 public class EmployeeWage {
 	public static void main(String[] args) {
-		int isFullTime = 1;
+		int isPartTime = 1;
+		int isFullTime = 2;
 		int empRatePerHr = 20;
 		int empWage = 0;
 		int empHrs = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		if (empCheck == isFullTime) {
 			empHrs = 8;
+		} else if (empCheck == isPartTime) {
+			empHrs = 4;
 		} else {
 			empHrs = 0;
 		}
@@ -16,4 +19,5 @@ public class EmployeeWage {
 		System.out.println("EmpWage :" + empWage);
 
 	}
+
 }
